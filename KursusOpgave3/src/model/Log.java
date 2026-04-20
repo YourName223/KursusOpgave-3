a19faaed4e901e870a73b19d65b05e8e824c2793
Lini Rose <75529247+LiniRose@users.noreply.github.com>
2026-04-20 11:48:48 +0200

    log

--- KursusOpgave3/src/model/Log.java
+++ KursusOpgave3/src/model/Log.java
@@ -1,19 +1,34 @@
 package model;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class Log
 {
-  private Log()
-  {
 
+  private static Log instance;
+  private String logFile = "app.log";
+
+  private Log() {}
+
+  public static Log getInstance() {
+    if (instance == null) {
+      instance = new Log();
+    }
+    return instance;
   }
 
-  public static Log getInstance()
-  {
+  public void addLog(String action) {
+    String time = LocalDateTime.now().toString();
+    String fullMessage = time + " | " + action;
 
-  }
+    System.out.println(fullMessage);
 
-  public void addLog(String action)
-  {
-
+    try (FileWriter writer = new FileWriter(logFile, true)) {
+      writer.write(fullMessage + "\n");
+    } catch (IOException e) {
+      System.err.println("Could not write to log file: " + logFile);
+    }
   }
 }
