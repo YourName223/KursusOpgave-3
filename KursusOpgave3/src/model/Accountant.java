77cc5608e378f94a4bbd786f7552a6be532a3551
YourName223 <oscarhnielsen@gmail.com>
2026-04-20 11:11:06 +0200

    Added most methods in accoutn king....

--- KursusOpgave3/src/model/Accountant.java
+++ KursusOpgave3/src/model/Accountant.java
@@ -1,5 +1,23 @@
 package model;
 
-public class Accountant
+public class Accountant implements Runnable
 {
+  private Door lock;
+
+  public Accountant(Door lock)
+  {
+    this.lock = lock;
+  }
+
+  @Override public void run()
+  {
+
+  }
+
+  private void countTreasureRoomValue()
+  {
+    ReadList readList = lock.acquireWrite();
+    Log log = Log.getInstance();
+    log.addLog(readList.toString());
+  }
 }
--- KursusOpgave3/src/model/Valuable.java
+++ KursusOpgave3/src/model/Valuable.java
@@ -2,4 +2,27 @@ package model;
 
 public class Valuable
 {
+  private int value;
+
+  private String type;
+
+  private Valuable()
+  {
+
+  }
+
+  public static Valuable getInstance()
+  {
+
+  }
+
+  public int getValue()
+  {
+
+  }
+
+  public String getType()
+  {
+
+  }
 }
