2d63f584b5fbe4bd24388709a1b1307f358a585c
Lini Rose <75529247+LiniRose@users.noreply.github.com>
2026-04-24 13:34:46 +0200

    mine

--- KursusOpgave3/src/model/Mine.java
+++ KursusOpgave3/src/model/Mine.java
@@ -1,5 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Mine
 {
+  private ArrayList<Valuable> valuableList;
+
+  public Mine()
+  {
+    valuableList = new ArrayList<Valuable>();
+
+    for (int i = 0; i < 1000; i++) {
+      valuableList.add(Valuable.getInstance(String.valueOf(i)));
+    }
+  }
+
+  public Valuable mineValuable()
+  {
+    if (valuableList.isEmpty()) {
+      throw new IllegalStateException("Mine is empty!");
+    }
+    return valuableList.remove(valuableList.size() - 1);
+  }
 }
