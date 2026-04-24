c390544bb496635f4fecd259c16fbc8211736d3b
YourName223 <oscarhnielsen@gmail.com>
2026-04-24 12:42:18 +0200

    Made proxy and reader/writer

--- KursusOpgave3/src/model/WriteProxy.java
+++ KursusOpgave3/src/model/WriteProxy.java
@@ -5,19 +5,29 @@ import java.util.ArrayList;
 public class WriteProxy implements ReadWriteList
 {
   private TreasureRoom list;
+  private Guardsman access;
 
   public WriteProxy(TreasureRoom list, Guardsman access)
   {
     this.list = list;
+    this.access = access;
   }
 
   @Override public void write(Valuable valuable)
   {
+    if(!access.hasWriteAccess(Thread.currentThread()))
+    {
+      throw new SecurityException("No read access");
+    }
     list.write(valuable);
   }
 
   @Override public ArrayList<Valuable> read()
   {
+    if(!access.hasReadAccess(Thread.currentThread()))
+    {
+      throw new SecurityException("No read access");
+    }
     return list.read();
   }
 }
