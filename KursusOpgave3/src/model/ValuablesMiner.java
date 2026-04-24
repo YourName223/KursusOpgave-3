5811897688fde8c99068c94ff6e7d59c803a017c
younesazimi21 <younesazimi21@gmail.com>
2026-04-24 12:49:57 +0200

    ValuablesMiner run metode tilføjet

--- KursusOpgave3/src/model/ValuablesMiner.java
+++ KursusOpgave3/src/model/ValuablesMiner.java
@@ -15,7 +15,21 @@ public class ValuablesMiner implements Runnable
 
   @Override public void run()
   {
+    while (true)
+    {
+      mineValuable();
+      depositValuable();
 
+      try
+      {
+        Thread.sleep((long)(Math.random() * 500 + 100));
+      }
+      catch (InterruptedException e)
+      {
+        Thread.currentThread().interrupt();
+        break;
+      }
+    }
   }
 
   private void mineValuable()
